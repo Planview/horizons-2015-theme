f11e21e11ee778c7539f4bb8de29e1edb9aff64c
CORPORATE\sebner <sebner@planview.com>
2017-02-27 13:44:47 -0600

    Updates and new files for Horizons 2017.

--- js/main.js
+++ js/main.js
@@ -25,3 +25,46 @@
     });
   });
 })(this, jQuery);
+
+jQuery(document).ready(function($){
+  var index = 2;
+  var totalNumberOfImages = 2;
+  function preload(arrayOfImages) {
+    $(arrayOfImages).each(function(){
+      $('<img/>')[0].src = this;
+    });
+  }
+  // image swapping script for the Call for Presentations page
+  function swapImageSource() {
+    $('#horizons-presenter').fadeOut(500,function(){
+      $(this).fadeIn(500)[0].src = '/wp-content/themes/horizons-2015/img/planview-horizons-presenter-' + index + '.jpg';
+      index == totalNumberOfImages ? index = 1 : index += 1;
+      setTimeout(swapImageSource, 5000);
+    });
+  }
+  if ( $('#horizons-presenter').length && $('#horizons-presenter').is(':visible') ) { //if exists and is visible (i.e. not the mobile view)
+    totalNumberOfImages = 6;
+    preload([
+      '/wp-content/themes/horizons-2015/img/planview-horizons-presenter-2.jpg',
+      '/wp-content/themes/horizons-2015/img/planview-horizons-presenter-3.jpg',
+      '/wp-content/themes/horizons-2015/img/planview-horizons-presenter-4.jpg',
+      '/wp-content/themes/horizons-2015/img/planview-horizons-presenter-5.jpg',
+      '/wp-content/themes/horizons-2015/img/planview-horizons-presenter-6.jpg'
+    ]);
+    setTimeout(swapImageSource, 5000);
+  }
+  // image swapping script for the Hotel & Travel page
+  function swapImageSourceVenue() {
+    $('#horizons-venue').fadeOut(500,function(){
+      $(this).fadeIn(500)[0].src = '/wp-content/uploads/sites/7/2017/02/fairmont-austin-hotel-' + index + '.jpg';
+      index == totalNumberOfImages ? index = 1 : index += 1;
+      setTimeout(swapImageSourceVenue, 5000);
+    });
+  }
+  if ( $('#horizons-venue').length ) { //if exists
+    preload([
+      '/wp-content/uploads/sites/7/2017/02/fairmont-austin-hotel-2.jpg'
+    ]);
+    setTimeout(swapImageSourceVenue, 5000);
+  }
+});
